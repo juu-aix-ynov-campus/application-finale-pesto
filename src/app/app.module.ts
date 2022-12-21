0100f3f27e834d81deb617ee84366eb56ebe30db
antony <antonypinel@gmail.com>
2022-12-21 21:47:34 +0100

    cart page complete with functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPage implements OnInit {
 
+  items = [
+    {
+      image: 'assets/img/item1.jpg',
+      name: 'Item 1',
+      description: 'This is a description of item 1',
+      price: 10,
+    },
+    {
+      image: 'assets/img/item2.jpg',
+      name: 'Item 2',
+      description: 'This is a description of item 2',
+      price: 20,
+    }
+  ];
+  price = 0;
+  delivery = 5;
+  total = 0;
+
   constructor() { }
 
   ngOnInit() {
+    this.calculateTotal();
   }
 
+  removeItem(item: any) {
+    const index = this.items.indexOf(item);
+    if (index > -1) {
+      this.items.splice(index, 1);
+      this.calculateTotal();
+    }
+  }
+
+  calculateTotal() {
+    this.price = 0;
+    this.items.forEach(item => {
+      this.price += item.price;
+    });
+    this.total = this.price + this.delivery;
+  }
+
+  openPaymentModal() {
+
+  }
 }
